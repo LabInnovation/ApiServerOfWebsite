4423ffe305677b490e1b873134a683331a83757e
Yi-Hsun Chou <Yi-HsunChou@Yi-Hsuns-MacBook-Air.local>
2016-11-10 17:09:24 +0800

    - Add 404 and 500 error message

--- app.js
+++ app.js
@@ -30,8 +30,20 @@ router.route('/apply')
     });
 
 
+
 // =============================================================================
+// Success
 app.use('/api', router);
+
+// Handle 404
+app.use(function(req, res) {
+  res.send('404: Page not Found', 404);
+});
+
+// Handle 500
+app.use(function(error, req, res, next) {
+  res.send('500: Internal Server Error', 500);
+});
 
 app.listen(port);
 console.log('Api server is already run on ' + port);
